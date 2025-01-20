4c0fdb5f1868a37873aade95b7c6a8903c83797d
Hero <hero4barca@gmail.com>
2025-01-20 10:13:18 +0100

    added pyscopg to requirements.txt

--- project/settings/staging.py
+++ project/settings/staging.py
@@ -1,17 +1,39 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
+import dj_database_url # postgre db_url
+
+
+# turn debug to false in production
 DEBUG = True
 
+# installed apps for prod only
+INSTALLED_APPS += [
+
+]
+
+# read secret key from environment variable
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# redirect all non secure requests to https
+SECURE_SSL_REDIRECT = True
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 try:
     from .local import *
 except ImportError:
     pass
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# set to use postgres backend in production
+DATABASES['default'] = dj_database_url.parse( os.environ.get("DATABASE_URL"),
+                                                 conn_max_age=600)
