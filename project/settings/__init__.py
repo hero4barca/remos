222130d6186ca48073dd3e26579aa614f7a7d4f3
Hero <hero4barca@gmail.com>
2025-01-20 19:15:59 +0100

    fixed env selection issues

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -1,22 +1,21 @@
 import os
 import sys
-from django.conf import settings
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the "env" variable from the environment, default to "dev"
-env = os.getenv('env', 'dev').lower()
+env_type = os.getenv('ENV_TYPE', 'dev').lower()
 
 try:
-    if env == 'staging':
+    if env_type == 'staging':
         from .staging import *  # Import settings from staging.py
-    elif env == 'production':
+    elif env_type == 'production':
         from .production import *  # Import settings from production.py
     else:
         from .dev import *  # Default to dev.py
 except ImportError as e:
     # If there's an import error, log it and fall back to dev.py
-    sys.stderr.write(f"Error importing settings for '{env}': {e}\n")
+    sys.stderr.write(f"Error importing settings for '{env_type}': {e}\n")
     from .dev import *
